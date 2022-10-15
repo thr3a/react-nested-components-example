19e2a037d412ddc23d0aa55f10de8e4043fe0697
thr3a <thr3a@example.com>
2022-10-15 18:25:53 +0900

    add:

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-```
-git clone https://github.com/thr3a/minimum-react-template.git
-```
-
-# Github Pagesを使う場合
-
-- package.jsonの `homepage` を適切に設定する
-- https://github.com/thr3a/minimum-react-template/settings/pages でBranchをgh-pagesにする
-- https://thr3a.github.io/minimum-react-template/
+https://beta.reactjs.org/learn/sharing-state-between-components
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import { MantineProvider, Text } from '@mantine/core';
+import { TextInput } from '@mantine/core';
+import { useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setText(e.target.value);
+  }
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
-    </MantineProvider>
+    <>
+      <MyInput
+        label="First input"
+        value={text}
+        onChange={handleChange}
+      />
+      <MyInput
+        label="Second input"
+        value={text}
+        onChange={handleChange}
+      />
+      { text }
+    </>
+  );
+}
+
+type Props = {
+  label: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const MyInput = ({ label, value, onChange }: Props) => {
+  return (
+    <TextInput value={value} onChange={onChange} label={label}>
+    </TextInput>
   );
 }
